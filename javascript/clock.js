274f42d07af2d4bc0cbbd9a985edc631556cd6e5
kd8lvt <kd8lvt@users.noreply.github.com>
2016-03-08 10:20:40 -0500

    Update clock.js

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,4 +1,7 @@
 var d = new Date
+function sleep(el) {
+  setTimeout(function() { foobar_cont(el); }, 5000);
+}
 function clock() {
   var date = d.getHours() + ":" + d.getMinutes()
   document.getElementById('clock').innerHTML = date
@@ -9,5 +12,6 @@ function startClock() {
   parent.removeChild(child);
   while (true) {
     clock()
+    sleep(1000)
   }
 }
