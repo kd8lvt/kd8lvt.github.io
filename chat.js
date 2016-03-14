132fc09ca35bfd58df7eb09496fca9eb2cb67b78
kd8lvt <kd8lvt@users.noreply.github.com>
2016-03-14 12:14:11 -0400

    Update chat.js

--- chat.js
+++ chat.js
@@ -17,6 +17,10 @@ var chat = document.getElementById('chat'),
 	defaultColors = ['rgb(255, 0, 0)','rgb(0, 0, 255)','rgb(0, 128, 0)','rgb(178, 34, 34)','rgb(255, 127, 80)','rgb(154, 205, 50)','rgb(255, 69, 0)','rgb(46, 139, 87)','rgb(218, 165, 32)','rgb(210, 105, 30)','rgb(95, 158, 160)','rgb(30, 144, 255)','rgb(255, 105, 180)','rgb(138, 43, 226)','rgb(0, 255, 127)'],
 	randomColorsChosen = {},
 	clientOptions = {
+			identity: {
+				username: "",
+				password: ""
+			},
 			options: {
 					debug: true
 				},
