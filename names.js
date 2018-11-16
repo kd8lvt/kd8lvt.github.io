330c03281e42551c3dadf728d2096b5d164dcc29
kd8lvt <kd8lvt@users.noreply.github.com>
2018-11-16 16:15:05 -0500

    Update names.js

--- names.js
+++ names.js
@@ -6,7 +6,7 @@ setInterval(function() {
 	var newDoc = document.body.innerHTML
 	if (oldDoc == newDoc) return;
 	for (var i=0;i<newDoc.length;i++) {
-		for (var j=0;k<origN.length;i++) {
+		for (var j=0;j<origN.length;i++) {
 			if (newN[j] != null) document.body.innerHTML.replace(orig[j],newN[j]);
 		}
 	}
