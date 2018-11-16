1722379b44e29a96d10f287afc5253fe593492d6
kd8lvt <kd8lvt@users.noreply.github.com>
2018-11-16 16:33:11 -0500

    Update names.js

--- names.js
+++ names.js
@@ -1,11 +1,12 @@
 var origN = ['Blue Snow Pup'];
 var newN = ['Ethan'];
 var classesToReplace = ['scrollerWrap-21JEkd','chat-3bRxxu'];
+var KD8LVT_RENAMELOOP;
 
 function KD8LVT_ENABLECLIENTRENAME() {
-	setInterval(function() {
+	KD8LVT_RENAMELOOP = setInterval(function() {
 		for (var k=0;k<classesToReplace.length;k++) {
-			var elem = document.getElementByClassName(classesToReplace[k]);
+			var elem = document.getElementsByClassName(classesToReplace[k])[0];
 			var newDoc = elem.innerHTML;
 			for (var i=0;i<newDoc.length;i++) {
 				for (var j=0;j<origN.length;i++) {
@@ -16,3 +17,7 @@ function KD8LVT_ENABLECLIENTRENAME() {
 	},100);
 }
  
+function KD8LVT_DISABLECLIENTRENAME() {
+	cancelInterval(KD8LVT_RENAMELOOP);
+	console.log("Disabled rename loop.");
+}
